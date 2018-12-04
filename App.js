b3939585f7dd933590da42ec2f6b9a0c645d65c8
Jerlyn Chai <jer@Jerlyns-MacBook-Air.local>
2018-12-04 22:09:57 +0800

    Update styles of homepage

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Avatar } from 'react-native-elements';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <Avatar
+      rounded
+      source={{uri: "https://cdn.pixabay.com/photo/2017/02/03/13/54/unicorn-2035174_960_720.png"}}
+      xlarge
+      onPress={() => console.log("Unicorn!")}
+      activeOpacity={0.5}/>
+      
+        <Text style={styles.bigblue}>Hellooooooooo!!!!</Text>
+        <Button title={'Jerlyn'} 
+        onPress={() => {console.log ('happy'); }} 
+        color="purple"
+        />
       </View>
     );
   }
@@ -14,8 +26,13 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFE4E1',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 20,
   },
 });
