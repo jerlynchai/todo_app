dcd0f63e6283658913b58264530ec004075b24d5
Jerlyn Chai <jer@Jerlyns-MacBook-Air.local>
2018-12-05 20:35:18 +0800

    Creates header component

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import Header from './component/Header';
 import { Avatar } from 'react-native-elements';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+      <Header textIwant={'byebye'} />
+      {/* <Header textIwant={'Hello World'} /> */}
       <Avatar
       rounded
       source={{uri: "https://cdn.pixabay.com/photo/2017/02/03/13/54/unicorn-2035174_960_720.png"}}
